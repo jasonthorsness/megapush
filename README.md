384bd09867f4795ebc46209e6e48314a7f88bc61
Jason Thorsness <thorsness@gmail.com>
2024-07-08 21:13:39 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # megapush
+
 Load Data At Wire Speed
+[https://www.jasonthorsness.com/8](https://www.jasonthorsness.com/8)
---
+++ main.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/schollz/progressbar/v3"
+	"io"
+	"math"
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	bufferSizeBytes    = 256 * 1024 * 1024
+	maxPayloadSize     = 1024 * 1024
+	maxTotalBufferSize = 255
+)
+
+type rowBatch struct {
+	count  int64
+	buffer *bytes.Buffer
+}
+
+func usage() {
+	fmt.Println("Usage:")
+	fmt.Println("megapush <buffer ram to use in GiB> host port user password database table <number of rows to insert> <bytes length of random payload in each row>")
+	fmt.Println("Example:")
+	fmt.Println(`megapush 8 svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com 3333 jason-tmp SrO0P2kCjumi1ZtRTlzT2cCqrsvnOVFY jtdb pushed 10000000 64`)
+}
+
+func main() {
+	err := mainInner()
+	if err != nil {
+		fmt.Println(err)
+		usage()
+		return
+	}
+}
+
+func mainInner() error {
+	if len(os.Args) != 10 {
+		return fmt.Errorf("invalid number of arguments")
+	}
+
+	totalBufferRaw := os.Args[1]
+	host := os.Args[2]
+	portRaw := os.Args[3]
+	user := os.Args[4]
+	password := os.Args[5]
+	database := os.Args[6]
+	table := os.Args[7]
+	numRowsRaw := os.Args[8]
+	randomPayloadSizeRaw := os.Args[9]
+
+	parsedTotalBuffer, err := strconv.ParseUint(totalBufferRaw, 10, 16)
+	if err != nil {
+		return err
+	}
+	if parsedTotalBuffer == 0 || parsedTotalBuffer > maxTotalBufferSize {
+		return fmt.Errorf("buffer_gib must be > 0 and <= " + strconv.FormatUint(maxTotalBufferSize, 10))
+	}
+	numBuffers := (int64(parsedTotalBuffer) * 1024 * 1024 * 1024) / bufferSizeBytes
+
+	parsedPort, err := strconv.ParseUint(portRaw, 10, 16)
+	if err != nil {
+		return err
+	}
+
+	parsedNumRows, err := strconv.ParseUint(numRowsRaw, 10, 64)
+	if err != nil {
+		return err
+	}
+	if parsedNumRows == 0 || parsedNumRows > math.MaxInt64 {
+		return fmt.Errorf("num_rows must be > 0 and <= 9223372036854775807")
+	}
+	numRows := int64(parsedNumRows)
+
+	parsedPayloadSize, err := strconv.ParseUint(randomPayloadSizeRaw, 10, 16)
+	if err != nil {
+		return err
+	}
+	if parsedPayloadSize == 0 || parsedPayloadSize > maxPayloadSize {
+		return fmt.Errorf("random_payload_size must be > 0 and <= " + strconv.FormatUint(maxPayloadSize, 10))
+	}
+	payloadSize := int64(parsedPayloadSize)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?tls=true", user, password, host, parsedPort, database)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+
+	rows, err := db.Query("SELECT @@SINGLESTOREDB_VERSION")
+	if err != nil {
+		return err
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(rows)
+
+	if !rows.Next() {
+		return fmt.Errorf("no rows")
+	}
+	version := ""
+	err = rows.Scan(&version)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Connected to SingleStore %s\n", version)
+
+	query := `DROP TABLE IF EXISTS ` + table
+	fmt.Println(query)
+	_, err = db.Exec(query)
+	if err != nil {
+		return err
+	}
+	query = `CREATE TABLE ` + table + `(
+	  documentID BIGINT NOT NULL,
+      payload LONGBLOB NOT NULL,
+      SORT KEY(documentID),
+      SHARD KEY(documentID))`
+	fmt.Println(query)
+	_, err = db.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	remaining := atomic.Int64{}
+	remaining.Store(numRows)
+	loaded := atomic.Int64{}
+
+	bufferReadyChannel := make(chan rowBatch, numBuffers)
+	bufferRecycleChannel := make(chan *bytes.Buffer, numBuffers)
+	wgGen := sync.WaitGroup{}
+
+	for i := int64(0); i < numBuffers; i++ {
+		bufferRecycleChannel <- &bytes.Buffer{}
+	}
+
+	batchSize := bufferSizeBytes / (2*int64(len(strconv.FormatInt(math.MaxInt64, 10))) + payloadSize + 3)
+
+	// Generator
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wgGen.Add(1)
+		go func() {
+			defer wgGen.Done()
+			numberBuffer := make([]byte, 0, 20)
+			payloadBuffer := make([]byte, payloadSize)
+			for newRemaining := remaining.Add(-batchSize); newRemaining > -batchSize; newRemaining = remaining.Add(-batchSize) {
+				buffer := <-bufferRecycleChannel
+				buffer.Reset()
+
+				from := newRemaining
+				count := batchSize
+
+				if newRemaining < 0 {
+					from = 0
+					count = batchSize + newRemaining
+				}
+
+				for i := from; i < from+count; i++ {
+					n, err := rand.Read(payloadBuffer)
+					if err != nil || n < len(payloadBuffer) {
+						panic(err)
+					}
+					numberBuffer = numberBuffer[:0]
+					numberBuffer = strconv.AppendInt(numberBuffer, i, 10)
+					buffer.Write(numberBuffer)
+					buffer.WriteByte(',')
+					for j := int64(0); j < payloadSize; j++ {
+						if payloadBuffer[j] == 0 || payloadBuffer[j] == ',' || payloadBuffer[j] == '\n' {
+							buffer.WriteByte(0)
+						}
+						buffer.WriteByte(payloadBuffer[j])
+					}
+					buffer.WriteByte('\n')
+				}
+				bufferReadyChannel <- rowBatch{count, buffer}
+			}
+		}()
+	}
+
+	go func() {
+		wgGen.Wait()
+		close(bufferReadyChannel)
+	}()
+
+	// Pusher
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func() {
+			readerName := strconv.FormatInt(int64(i), 10)
+			for {
+				batch, ok := <-bufferReadyChannel
+				if !ok {
+					return
+				}
+				mysql.RegisterReaderHandler(readerName, func() io.Reader { return batch.buffer })
+				query := "LOAD DATA LOCAL INFILE 'Reader::" + readerName + "' INTO TABLE " + table + " FIELDS TERMINATED BY ',' ESCAPED BY '\000' LINES TERMINATED BY '\n'"
+				_, err := db.Exec(query)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				mysql.DeregisterReaderHandler(readerName)
+				loaded.Add(payloadSize * batch.count)
+				bufferRecycleChannel <- batch.buffer
+			}
+		}()
+	}
+
+	bar := progressbar.DefaultBytes(numRows * payloadSize)
+	for {
+		err := bar.Set64(loaded.Load())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if remaining.Load() <= 0 && len(bufferRecycleChannel) == int(numBuffers) {
+			err = bar.Finish()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			break
+		}
+		time.Sleep(250 * time.Millisecond)
+	}
+
+	fmt.Println("done")
+	return nil
+}
